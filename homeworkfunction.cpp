2460ab2e26e115021512279523318771acf9fed0
Panukul <52194214+Panukul@users.noreply.github.com>
2019-08-27 14:40:38 +0700

    Add files via upload

---
+++ homeworkfunction.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <time.h>
+using namespace std;
+int checkNum(int,int);
+int main() 
+{
+    int num,gue=0;
+    srand (time(NULL));
+    int random=rand()%10+1;
+    cout<<"###Welcome to guessing number game###\n";
+    cout<<"Secret number has been chosen\n";
+    do{
+        cout<<"Guess the number (1 to 10): ";
+        cin>>num;
+        gue++;
+        checkNum(random,num);
+    }while(random!=num);
+
+    cout<<"\nCongratulations!\n";
+    cout<<"The secret number is "<<random<<"\n";
+    cout<<"You made "<<gue<<" guesses\n";
+    return (0);
+}
+
+int checkNum(int c,int n)
+{
+    if(c>n)
+	{
+        cout<<"The secret number is lower\n\n" ;
+	}
+    else if(c<n)
+	{
+        cout<<"The secret number is higher\n\n";
+	}
+    return (c,n);
+}
